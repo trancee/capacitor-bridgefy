7cc2d0b5b7c3976017fe36f47708b1e412fe6ee1
Philipp Grosswiler <philipp.grosswiler@gmail.com>
2025-08-16 18:12:30 +0200

    fix(android): missing reason field in failures

--- android/src/main/java/com/getcapacitor/community/classes/events/FailToStopEvent.java
+++ android/src/main/java/com/getcapacitor/community/classes/events/FailToStopEvent.java
@@ -1,11 +1,21 @@
 package com.getcapacitor.community.classes.events;
 
 import androidx.annotation.NonNull;
+import com.getcapacitor.JSObject;
 import com.getcapacitor.community.BridgefyController.Reason;
 
 public class FailToStopEvent extends FailToEvent {
 
     public FailToStopEvent(@NonNull Reason reason) {
         super(reason);
+    }
+
+    @NonNull
+    public JSObject toJSObject() {
+        JSObject result = new JSObject();
+
+        result.put("reason", super.toJSObject());
+
+        return result;
     }
 }
